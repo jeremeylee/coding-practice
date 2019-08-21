bed5c167611c8dba6ce58ec1c81f8602a2271763
Jeremey Lee <leejeremey@gmail.com>
2019-08-21 16:59:12 -0700

    isPalindrome list Time: O(n) Space: O(1)

--- src/linkedlists/PalindromeList.java
+++ src/linkedlists/PalindromeList.java
@@ -25,4 +25,40 @@ public class PalindromeList {
         }
         return true;
     }
+
+    public boolean isPalindrome2(ListNode head) {
+        int size = 0;
+        ListNode findSize = head;
+        while (findSize != null) {
+            size++;
+            findSize = findSize.next;
+        }
+        if (size < 2) {
+            return true;
+        }
+        ListNode curr = head;
+        ListNode prev = null;
+
+        for (int i = 0; i < size/2; i++) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        if (size % 2 == 1) {
+            curr = curr.next;
+        }
+
+        for (int i = 0; i < size/2; i++) {
+            if (prev.val != curr.val) {
+                return false;
+            } else {
+                prev = prev.next;
+                curr = curr.next;
+            }
+        }
+
+        return true;
+    }
 }

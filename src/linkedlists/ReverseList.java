16f0eff0e2cf77dd73269a7d9de45ce5ae65d44b
Jeremey Lee <leejeremey@gmail.com>
2019-08-21 14:34:20 -0700

    Iterative reverse linked list

---
+++ src/linkedlists/ReverseList.java
@@ -0,0 +1,17 @@
+package linkedlists;
+
+public class ReverseList {
+    public ListNode iterativeReverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
+}

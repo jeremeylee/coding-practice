fb6b15ecb3bdfb9733deef9bf9d8557e6ab25f8f
Jeremey Lee <leejeremey@gmail.com>
2019-08-21 16:26:18 -0700

    Check cyclic with runners

--- src/linkedlists/CyclicList.java
+++ src/linkedlists/CyclicList.java
@@ -15,4 +15,22 @@ public class CyclicList {
         return false;
 
     }
+
+    public boolean hasCycle2(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+}
 }

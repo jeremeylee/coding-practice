24d93d5610f8e89caf4eff02b39a40fb408511a5
Jeremey Lee <leejeremey@gmail.com>
2019-08-22 15:45:12 -0700

    One pass two sum solution

---
+++ src/arrays/TwoSum.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numbers = new HashMap<Integer,Integer>();
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if(numbers.containsKey(complement)) {
+                return new int[]{ numbers.get(complement), i};
+            }
+
+            numbers.put(nums[i], i);
+
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}

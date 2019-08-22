dac8524433f573b52f166316b28fa16e0acfffa1
Jeremey Lee <leejeremey@gmail.com>
2019-08-22 16:00:03 -0700

    Calculate best time to buy and sell stock

--- src/arrays/BestStockTime.java
+++ src/arrays/BestStockTime.java
@@ -22,4 +22,19 @@ public class BestStockTime {
         }
 
     }
+    public int maxProfitOnePass(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+
 }

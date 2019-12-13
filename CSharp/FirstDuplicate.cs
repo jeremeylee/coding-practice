a072f8117ab8edcd09fbe2eea063d396c31b4147
Jeremey Lee <leejeremey@gmail.com>
2019-12-12 19:08:56 -0800

    FirstDuplicate

--- CSharp/FirstDuplicate.cs
+++ CSharp/FirstDuplicate.cs
@@ -8,5 +8,22 @@ namespace Practice.CSharp
 {
     class FirstDuplicate
     {
-    }
+        int firstDuplicate(int[] a)
+        {
+            HashSet<int> duplicate = new HashSet<int>();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (duplicate.Contains(a[i]))
+                {
+                    return a[i];
+                }
+                else
+                {
+                    duplicate.Add(a[i]);
+                }
+            }
+
+            return -1;
+        }
 }

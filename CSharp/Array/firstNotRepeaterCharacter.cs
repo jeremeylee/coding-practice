f38eb9867201c2fb84871bd7d43a7d334ac9af1b
Jeremey Lee <leejeremey@gmail.com>
2019-12-14 21:52:48 -0800

    First not repeating character

---
+++ CSharp/Array/firstNotRepeaterCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Array
+{
+    class firstNotRepeaterCharacter
+    {
+        char firstNotRepeatingCharacter(string s)
+        {
+            char[] characters = s.ToCharArray();
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                if (Array.IndexOf(characters, characters[i]) == Array.LastIndexOf(characters, characters[i]))
+                {
+                    return characters[i];
+                }
+            }
+
+            return '_';
+        }
+
+    }
+}

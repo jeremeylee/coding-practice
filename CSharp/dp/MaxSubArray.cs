57b46ab1009ef077a8983ca1ee6df0886054d479
Jeremey Lee <leejeremey@gmail.com>
2019-12-16 14:16:08 -0800

    Finds the largest contiguous subarray

---
+++ CSharp/dp/MaxSubArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.dp
+{
+    class MaxSubArray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int max = nums[0];
+            int currMax = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                currMax = Math.Max(currMax + nums[i], nums[i]);
+                max = Math.Max(max, currMax);
+            }
+            return max;
+        }
+    }
+}

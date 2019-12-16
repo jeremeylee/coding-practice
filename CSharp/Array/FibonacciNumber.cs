7980721bc69feed92f15d1fdd48827710d8ea21f
Jeremey Lee <leejeremey@gmail.com>
2019-12-15 21:52:37 -0800

    Calculate the N fibonacci number

---
+++ CSharp/Array/FibonacciNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Array
+{
+    class FibonacciNumber
+    {
+        public int Fib(int N)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < N; i++)
+            {
+                int c = a + b;
+                b = a;
+                a = c;
+            }
+
+            return a;
+        }
+    }
+}

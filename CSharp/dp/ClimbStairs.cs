3626486b7589f7bf186db554276245863565df8b
Jeremey Lee <leejeremey@gmail.com>
2019-12-16 16:57:03 -0800

    climbing stairs

---
+++ CSharp/dp/ClimbStairs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.dp
+{
+    class ClimbStairs
+    {
+        public int ClimbStairs(int n)
+        {
+            int a = 1;
+            int b = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int c = a + b;
+                b = a;
+                a = c;
+            }
+
+            return a;
+        }
+    }
+}

1a5da9b6950afc244efe7f1d9990c306edb1a8f3
Jeremey Lee <leejeremey@gmail.com>
2019-12-18 14:01:41 -0800

    Check if an array contains duplicate numbers

---
+++ CSharp/Array/ContainsDuplicate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Array
+{
+    class ContainsDuplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> unique = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (unique.Contains(nums[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    unique.Add(nums[i]);
+                }
+            }
+
+            return false;
+        }
+    }
+}

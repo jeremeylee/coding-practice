8c07878fdad1876a8970164533d8b88f4f655435
Jeremey Lee <leejeremey@gmail.com>
2019-12-18 16:15:54 -0800

    Checks if 2 binary search trees are the same

---
+++ CSharp/Trees/IsSameTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Trees
+{
+    class IsSameTree
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if ((p == null && q != null) || p != null && q == null)
+            {
+                return false;
+            }
+
+            if (p == null && q == null)
+            {
+                return true;
+            }
+
+            if (p.val != q.val)
+            {
+                return false;
+            }
+
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}

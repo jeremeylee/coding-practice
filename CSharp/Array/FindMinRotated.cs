dadf0fd06414337257edd223e1a17f389fbf9402
Jeremey Lee <leejeremey@gmail.com>
2019-12-18 16:00:07 -0800

    Find the minimum value in a rotated sorted array

---
+++ CSharp/Array/FindMinRotated.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Array
+{
+    class FindMinRotated
+    {
+        public int FindMin(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            int end = nums.Length - 1;
+            int start = 0;
+
+            if (nums[end] > nums[0])
+            {
+                return nums[0];
+            }
+
+            while (start <= end)
+            {
+                int mid = start + (end - start) / 2;
+
+                if (nums[mid] > nums[mid + 1])
+                {
+                    return nums[mid + 1];
+                }
+
+                if (nums[mid] < nums[mid - 1])
+                {
+                    return nums[mid];
+                }
+
+                if (nums[mid] > nums[0])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+
+            return 0;
+        }
+    }
+}

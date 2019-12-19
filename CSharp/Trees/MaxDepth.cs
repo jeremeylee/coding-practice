df83ef5445bed1709bc12e02e663a253fe98266d
Jeremey Lee <leejeremey@gmail.com>
2019-12-18 16:11:49 -0800

    Find the max depth of a binary search tree

---
+++ CSharp/Trees/MaxDepth.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Trees
+{
+    class MaxDepth
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+
+        }
+    }
+}

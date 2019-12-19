54636840f94c771abfe7dc6524ed6612c917d560
Jeremey Lee <leejeremey@gmail.com>
2019-12-18 16:44:29 -0800

    invert a binary tree

---
+++ CSharp/Trees/InvertTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Trees
+{
+    class InvertTree
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            TreeNode temp = root.left;
+            root.left = InvertTree(root.right);
+            root.right = InvertTree(temp);
+            return root;
+        }
+    }
+}

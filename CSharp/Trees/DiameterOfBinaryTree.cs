05359559d6e45980932e8fb1a87b7e81ad495a4c
Jeremey Lee <leejeremey@gmail.com>
2019-12-20 00:05:02 -0800

    Longest path between two nodes in a binary tree

---
+++ CSharp/Trees/DiameterOfBinaryTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Trees
+{
+    class DiameterOfBinaryTree
+    {
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int height = TreeHeight(root.left) + TreeHeight(root.right);
+            return Math.Max(height, Math.Max(DiameterOfBinaryTree(root.left), DiameterOfBinaryTree(root.right)));
+        }
+
+        public int TreeHeight(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(TreeHeight(root.left), TreeHeight(root.right)) + 1;
+        }
+    }
+}

a2b881fb3da36f5acaa1144dc011a090c982cf2e
Jeremey Lee <leejeremey@gmail.com>
2019-12-19 23:49:16 -0800

    Merged 2 linked lists

--- CSharp/LinkedLists/mergeTwoLinkedLists.cs
+++ CSharp/LinkedLists/mergeTwoLinkedLists.cs
@@ -7,54 +7,40 @@ using System.Threading.Tasks;
 namespace Practice.CSharp.LinkedLists
 {
     class mergeTwoLinkedLists
-    {// Singly-linked lists are already defined with this interface:
-     // class ListNode<T> {
-     //   public T value { get; set; }
-     //   public ListNode<T> next { get; set; }
-     // }
-     //
-        ListNode<int> mergeTwoLinkedLists(ListNode<int> l1, ListNode<int> l2)
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
         {
-            if (l1 == null && l2 == null)
-            {
-                return null;
-            }
-
-            ListNode<int> merged = new ListNode<int>();
-            ListNode<int> head = merged;
-
+            ListNode merged = new ListNode(0);
+            ListNode head = merged;
             while (l1 != null && l2 != null)
             {
-                if (l1.value >= l2.value)
+                if (l1.val >= l2.val)
                 {
-                    merged.value = l2.value;
+                    merged.next = l2;
                     l2 = l2.next;
                 }
                 else
                 {
-                    merged.value = l1.value;
+                    merged.next = l1;
                     l1 = l1.next;
                 }
 
-                merged.next = new ListNode<int>();
                 merged = merged.next;
             }
 
             if (l1 != null)
             {
-                Console.WriteLine("l1");
-                merged.value = l1.value;
-                merged.next = l1.next;
+                merged.next = l1;
+                merged = merged.next;
             }
 
             if (l2 != null)
             {
-                Console.WriteLine("l2");
-                merged.value = l2.value;
-                merged.next = l2.next;
+                merged.next = l2;
+
             }
 
-            return head;
+            return head.next;
         }
 
     }

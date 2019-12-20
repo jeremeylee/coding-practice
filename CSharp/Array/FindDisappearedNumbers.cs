f38f842c96a6af49e75c500505def061cfdbf844
Jeremey Lee <leejeremey@gmail.com>
2019-12-19 23:39:32 -0800

    Find the missing numbers in a 1 <= a[i] <= n array

---
+++ CSharp/Array/FindDisappearedNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Array
+{
+    class FindDisappearedNumbers
+    {
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            int[] count = new int[nums.Length + 1];
+            List<int> res = new List<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                count[nums[i]]++;
+            }
+
+            for (int i = 1; i < count.Length; i++)
+            {
+                if (count[i] == 0)
+                {
+                    res.Add(i);
+                }
+            }
+
+            return res;
+        }
+    }
+}

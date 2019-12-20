ecf755a13ec2c3b761f8fa2c4095fa0d4300e262
Jeremey Lee <leejeremey@gmail.com>
2019-12-20 14:58:59 -0800

    get the intersection of two linked lists

---
+++ CSharp/LinkedLists/GetIntersectionNode.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.LinkedLists
+{
+    class GetIntersectionNode
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            int ALength = GetLength(headA);
+            int BLength = GetLength(headB);
+            if (ALength > BLength)
+            {
+                while (ALength > BLength)
+                {
+                    headA = headA.next;
+                    ALength--;
+                }
+            }
+            else if (ALength < BLength)
+            {
+                while (BLength > ALength)
+                {
+                    headB = headB.next;
+                    BLength--;
+                }
+            }
+
+            while (headA != headB && headA != null && headB != null)
+            {
+                headA = headA.next;
+                headB = headB.next;
+            }
+
+            return headA == headB ? headA : null;
+
+        }
+
+        public int GetLength(ListNode head)
+        {
+            int length = 0;
+            while (head != null)
+            {
+                head = head.next;
+                length++;
+            }
+
+            return length;
+        }
+    }
+}

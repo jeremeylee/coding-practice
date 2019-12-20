7bfc9f4d7b72a39574f80fb8fda18d6333df99a0
Jeremey Lee <leejeremey@gmail.com>
2019-12-20 14:13:09 -0800

    Linked list cycle

---
+++ CSharp/LinkedLists/HasCycle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.LinkedLists
+{
+    class HasCycle
+    {
+        public bool HasCycle(ListNode head)
+        {
+            ListNode fast = head;
+            ListNode slow = head;
+
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (fast == slow)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}

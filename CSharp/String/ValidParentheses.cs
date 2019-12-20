01205b479e730a1b668df964773d79e6f4ec38d9
Jeremey Lee <leejeremey@gmail.com>
2019-12-20 14:32:25 -0800

    Valid parentheses

---
+++ CSharp/String/ValidParentheses.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.String
+{
+    class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> parens = new Stack<char>();
+            Dictionary<char, char> parentheses = new Dictionary<char, char>();
+            parentheses.Add(')', '(');
+            parentheses.Add(']', '[');
+            parentheses.Add('}', '{');
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (parentheses.ContainsKey(s[i]))
+                {
+                    if (parens.Count() == 0 || parentheses[s[i]] != parens.Peek())
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        parens.Pop();
+                    }
+                }
+                else
+                {
+                    parens.Push(s[i]);
+                }
+            }
+
+            return parens.Count() > 0 ? false : true;
+        }
+    }
+}

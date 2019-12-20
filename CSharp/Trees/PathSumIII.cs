e5f3952b58074f5e56730db255e84798919bc3dd
Jeremey Lee <leejeremey@gmail.com>
2019-12-20 13:35:05 -0800

    Number of paths that sum to a given value in a tree

---
+++ CSharp/Trees/PathSumIII.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Trees
+{
+    class PathSumIII
+    {
+        public int PathSum(TreeNode root, int sum)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int count = helper(root, sum);
+            return PathSum(root.left, sum) + PathSum(root.right, sum) + count;
+        }
+
+        public int helper(TreeNode root, int sum)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int count = 0;
+            if (root.val == sum)
+            {
+                count++;
+            }
+
+            return count + helper(root.left, sum - root.val) + helper(root.right, sum - root.val);
+        }
+    }
+}

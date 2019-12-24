d7811c186641f60f581434c1f535e6819c57106b
Jeremey Lee <leejeremey@gmail.com>
2019-12-23 16:54:39 -0800

    Add two linked lists' digits

---
+++ CSharp/LinkedLists/AddTwoNumbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.LinkedLists
+{
+    class AddTwoNumbers
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode output = new ListNode(0);
+            ListNode head = output;
+            int carry = 0;
+
+            while (l1 != null || l2 != null)
+            {
+                if (l1 != null)
+                {
+                    carry += l1.val;
+                    l1 = l1.next;
+                }
+
+                if (l2 != null)
+                {
+                    carry += l2.val;
+                    l2 = l2.next;
+                }
+                output.val += carry;
+                carry = output.val / 10;
+                output.val %= 10;
+                Console.WriteLine(output.val);
+                if (l1 != null || l2 != null)
+                {
+                    output.next = new ListNode(0);
+                    output = output.next;
+                }
+
+            }
+            if (carry > 0)
+            {
+                output.next = new ListNode(carry);
+                output = output.next;
+            }
+            return head;
+
+        }
+    }
+}

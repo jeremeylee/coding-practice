320368ab175d0858f2ed88e64dbd1ebddda3a059
Jeremey Lee <leejeremey@gmail.com>
2019-12-23 19:48:53 -0800

    subarray sum equals k

---
+++ CSharp/Array/SubarraySum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Array
+{
+    class SubarraySum
+    {
+        //Unoptimized
+        public int SubarraySum(int[] nums, int k)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < nums.Length; j++)
+                {
+                    sum += nums[j];
+                    if (sum == k)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}

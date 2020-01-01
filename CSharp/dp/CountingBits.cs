5efc4dbfc039af5f4c3d52cfcce2f1d86b667918
Jeremey Lee <leejeremey@gmail.com>
2020-01-01 14:38:05 -0800

    Count 1's in bits

---
+++ CSharp/dp/CountingBits.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.dp
+{
+    class CountingBits
+    {
+        public int[] CountBits(int num)
+        {
+            int[] output = new int[num + 1];
+            for (int i = num; i >= 0; i--)
+            {
+                int x = i;
+                while (x > 0)
+                {
+                    if (x % 2 == 1)
+                    {
+                        output[i]++;
+                    }
+
+                    x /= 2;
+                }
+            }
+
+            return output;
+        }
+    }
+}

04d33b422d6140a55bbfcbd71516982c20fe863c
Jeremey Lee <leejeremey@gmail.com>
2020-01-02 12:46:07 -0800

    inorder binary tree traversal

---
+++ CSharp/Trees/InorderTraversal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.Trees
+{
+    class InorderTraversal
+    {
+        public List<int> inorder = new List<int>();
+
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null)
+            {
+                return inorder;
+            }
+
+            InorderTraversal(root.left);
+            inorder.Add(root.val);
+            InorderTraversal(root.right);
+            return inorder;
+        }
+    }
+}

ffe5c5009fdd8dc53e463fb107993d3acf94eb80
Jeremey Lee <leejeremey@gmail.com>
2020-01-03 15:51:23 -0800

    Generate parentheses

---
+++ CSharp/String/GenerateParentheses.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.String
+{
+    class GenerateParentheses
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            IList<string> list = new List<string>();
+            helper(n, list, "", 0, 0);
+            return list;
+        }
+
+        public void helper(int n, IList<string> list, string tempString, int open, int close)
+        {
+            if (tempString.Length == n * 2)
+            {
+                list.Add(tempString);
+                return;
+            }
+
+            if (open < n)
+            {
+                helper(n, list, tempString + "(", open + 1, close);
+            }
+            if (close < open)
+            {
+                helper(n, list, tempString + ")", open, close + 1);
+            }
+            return;
+        }
+    }
+}

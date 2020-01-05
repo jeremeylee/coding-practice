136acd1ba81f417e222fe3c5cab5f41569fc5315
Jeremey Lee <leejeremey@gmail.com>
2020-01-04 20:34:10 -0800

    palindromic substrings

---
+++ CSharp/String/PalindromicSubstrings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.String
+{
+    class PalindromicSubstrings
+    {
+        public int CountSubstrings(string s)
+        {
+            if (s == null || s.Length == 1)
+            {
+                return 1;
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                count += expand(s, i, i);
+                count += expand(s, i, i + 1);
+            }
+
+            return count;
+        }
+
+        public int expand(string s, int left, int right)
+        {
+            int count = 0;
+            while (left >= 0 && right < s.Length && (s[left] == s[right]))
+            {
+                count++;
+                left--;
+                right++;
+            }
+
+            return count;
+        }
+    }
+}

f15977548886d225382ef18829914524ffb16117
Jeremey Lee <leejeremey@gmail.com>
2020-01-14 20:29:31 -0800

    Group anagrams

---
+++ CSharp/String/GroupAnagrams.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.CSharp.String
+{
+    class GroupAnagrams
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            List<IList<string>> res = new List<IList<string>>();
+
+            for (int i = 0; i < strs.Length; i++)
+            {
+                char[] charArray = strs[i].ToCharArray();
+                Array.Sort(charArray);
+                string str = new string(charArray);
+                if (!map.ContainsKey(str))
+                {
+                    map.Add(str, res.Count);
+                    res.Add(new List<string>());
+                }
+
+                res[map[str]].Add(strs[i]);
+            }
+
+            return res;
+
+        }
+    }
+}
